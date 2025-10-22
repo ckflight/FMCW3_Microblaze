c2fd72cb5c325fc0c6f2ed31468f9ed246e68eba
ckflight <cnkkskn001@gmail.com>
2025-10-22 15:50:10 +0300

    pin definitons and comment according to the lates fpga code

--- FMCW3/src/definitions.h
+++ FMCW3/src/definitions.h
@@ -7,8 +7,9 @@
 #define ADF_CE_PIN          0   // GPIO[0] -> CE
 #define ADF_LE_PIN          1   // GPIO[1] -> LE
 
-#define RAMP_CONFIGURED     2   // GPIO[2] -> Ramp is configured --> to FPGA
-#define SAMPLING_DONE       3   // GPIO[3] -> Sampling is done --> to FPGA
+#define SAMPLING_DONE       2   // GPIO[2] -> Sampling is done --> to FPGA
+#define RAMP_CONFIGURED     3   // GPIO[3] -> Ramp is configured --> to FPGA
+#define SOFTWARE_RESET      4   // GPIO[4] -> Software reset for next config and radar op --> to FPGA
 
 #endif // DEFINITIONS_H
 
